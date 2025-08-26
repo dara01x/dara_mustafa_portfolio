9ed8119e76d2c48f6d234984b162e54bb3aff5cf
dara01x <daramistefa30@gmail.com>
2025-08-26 22:10:52 +0300

    Fix custom domain configuration: Update base path and add CNAME file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dara_mustafa_portfolio/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
